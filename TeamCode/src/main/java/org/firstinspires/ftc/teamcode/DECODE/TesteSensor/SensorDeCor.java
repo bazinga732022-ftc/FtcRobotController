004f781a3abf12405c021d874c8f67aca7f26a92
bazinga732022-ftc <bazinga73.2022@gmail.com>
2025-10-15 07:31:26 -0300

    Sensores
    Essa é outra classe de configuração de sensores mais atualizada.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/TesteSensor/SensorDeCor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/TesteSensor/SensorDeCor.java
@@ -17,12 +17,18 @@ public class SensorDeCor {
         cor1 = hw.get(NormalizedColorSensor.class,"cor1");
         cor1.setGain(19);
     }
+
+    //Detectad porque detected nao funfa. nao pergunte o pq
     public enum DetectadColor{
         RED,
 
         BLUE,
 
         YELLOW,
+
+        PURPLE,
+
+        VERBOLA,
 
         UNKNOWN
     }
@@ -41,15 +47,17 @@ public class SensorDeCor {
         /*red,green,blue;
         RED = Verm = < 0.6 , Az < 0.14, Verd < 0.22
         YELLOW = Verm < 0.800, Az < 0.2, Verd < 1.2
-        BLUE = Verm < 0.12, Az < 0.45, Verd < 0.21 =
+        BLUE = Verm < 0.12, Az < 0.45, Verd < 0.21
+        PURPLE = R < 1700, G < 2000, B < 3020
+        GBal =  r < .99, G < .3, B < .25
          */
       // TODO!!: calibração das cores: calibre conforme precisar
       if (normRed < 0.3 && normBlue < 0.14 && normGreen < 0.22) {
           return DetectadColor.RED;
-      } else if (normRed < .12 && normBlue < .45 && normGreen < 0.21) {
-          return DetectadColor.BLUE;
-      } else if (normRed < .8 && normBlue < .2 && normGreen < 1.2) {
-          return DetectadColor.YELLOW;
+      } else if (normRed < .17 && normBlue < .320 && normGreen < .2 && normGreen < .15) {
+          return DetectadColor.PURPLE;
+      } else if (normRed < .99 && normBlue < .3 && normGreen < .25 && normGreen > .21) {
+          return DetectadColor.VERBOLA;
       }
       else {
           return DetectadColor.UNKNOWN;
