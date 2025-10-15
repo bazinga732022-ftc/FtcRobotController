88f433d502645dc1f9c7b4bcc669e3719cf2806f
bazinga732022-ftc <bazinga73.2022@gmail.com>
2025-10-15 07:21:27 -0300

    nao utilizada
    classes de programção vazias

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codigosOffSeason/Robo_pele/calibracaoDaOdomo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codigosOffSeason/Robo_pele/calibracaoDaOdomo.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.codigosOffSeason.Robo_pele;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+@Disabled
 @Autonomous
 public class calibracaoDaOdomo extends LinearOpMode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codigosOffSeason/Robo_pele/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/codigosOffSeason/Robo_pele/intake.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode.codigosOffSeason.Robo_pele;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-
+@Disabled
 @TeleOp
 public class intake extends LinearOpMode {
 
