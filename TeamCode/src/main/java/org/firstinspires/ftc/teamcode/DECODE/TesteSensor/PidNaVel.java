87860ac68c11d02da81c3d251aaf60f606bbed39
bazinga732022-ftc <bazinga73.2022@gmail.com>
2025-10-20 15:44:08 -0300

    Essa classe foi feita para testar um sistema que aplica o PID em um motor DC para que nao ocorra perda de potencia. -edu pra joao pedro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DECODE/TesteSensor/PidNaVel.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.DECODE.TesteSensor;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp
+public class PidNaVel extends LinearOpMode {
+    public DcMotor teste;
+    public int CPR = 28;
+    public double ticksAntigos, kp, ki, kd, kf, RPM, DesejoRPM;
+    @Override
+    public void runOpMode() {
+        DesejoRPM = 330;
+        PIDFController pidf = new PIDFController(kp, ki, kd, kf);
+        teste = hardwareMap.get(DcMotor.class, "testes");
+        teste.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        teste.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+        while(opModeIsActive()){
+            double erro = pidf.calculate(RPM, DesejoRPM);
+            calculaRPM();
+            telemetry.update();
+            teste.setPower(.6 + erro);
+        }
+    }
+    public void calculaRPM(){
+        double ticksAtuais = teste.getCurrentPosition();
+        RPM = (ticksAtuais - ticksAntigos) / CPR * 60;
+        telemetry.addData("RPM", RPM);
+
+        ticksAntigos = ticksAtuais;
+        sleep(100);
+    }
+}
